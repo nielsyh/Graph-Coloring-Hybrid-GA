59b98db6dbd5eb336d6e165cde97dbb59096749c
N.Y Hendrikx <n.y.hendrikx@students.uu.nl>
2018-03-13 14:04:44 +0100

    setup...

---
+++ EC_Practicum_2/CrossoverFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC_Practicum_2
+{
+    class CrossoverFunctions
+    {
+
+        public CrossoverFunctions() {
+            //TODO: implement
+        }
+
+
+        public void GreedyPartitioningCrossover()
+        {
+            //TODO: implement
+        }
+
+        
+        public void VertexDescentLocalSearch()
+        {
+            //TODO: implement
+        }
+    }
+}
